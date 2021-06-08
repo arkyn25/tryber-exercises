b3504c382a35ed2e2a118c7f20c2d5bc8900fba4
Arlen Freitas <arlenfreitas1@gmail.com>
2021-06-08 13:52:52 -0300

    add exercise got

---
+++ front-end/gerenciar-estado-redux/bloco_16/dia_4/exercise-game-of-thrones-characters/src/components/CharacterInfo.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import propTypes from 'prop-types';
+
+class CharacterInfo extends React.Component {
+  render() {
+    //faça a desestruturação das props aqui
+    const { loading, character, error } = this.props;
+    if (!loading && character) {
+      return (
+        <ul>
+          <li>Name: {character.name}</li>
+          <li>Gender: {character.gender}</li>
+          <li>Aliases: {character.aliases.map((alias, index) => <p key={`${alias}-${index}`}>{alias}</p>)}</li>
+          <li>Books: {character.books.map((book, index) => <p key={`${book}-${index}`}>{book}</p>)}</li>
+        </ul>
+      )
+    }
+    if (error) { return <div>{error}</div>; }
+    if (loading) { return <div>Loading...</div>; }
+    return <div>Type a character name and click to search!</div>;
+  }
+};
+
+//mapeie o estado global para a propriedade da sua aplicação
+const mapStateToProps = (state) => ({ 
+  loading: state.characterReducer.loading,
+  character: state.characterReducer.character,
+  error: state.characterReducer.error,
+})
+
+// conecte este componente ao redux
+export default connect(mapStateToProps)(CharacterInfo);
+
+CharacterInfo.propTypes = {
+  loading: propTypes.bool.isRequired,
+  character: propTypes.arrayOf(propTypes.object),
+  error: propTypes.string,
+};
+
+CharacterInfo.defaultProps = {
+  character: null,
+  error: null,
+};
---
+++ front-end/gerenciar-estado-redux/bloco_16/dia_4/exercise-game-of-thrones-characters/src/reducers/index.js
@@ -0,0 +1,8 @@
+// src/reducers/index.js
+import { combineReducers } from 'redux';
+import characterReducer from './characterReducer';
+
+const rootReducer = combineReducers({
+  characterReducer});
+
+export default rootReducer;
