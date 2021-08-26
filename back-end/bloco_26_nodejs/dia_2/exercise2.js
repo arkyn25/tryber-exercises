3d05f94127887d0d523d40dded95746b76e38a18
Arlen Freitas <arlenfreitas1@gmail.com>
2021-08-26 16:22:11 -0300

    exercise 2

--- back-end/bloco_26_nodejs/dia_2/exercise2.js
+++ back-end/bloco_26_nodejs/dia_2/exercise2.js
@@ -1,6 +1,6 @@
 const fs = require('fs').promises;
 
-// Crie uma função que leia todos os dados do arquivo e imprima cada personagem no formato id - Nome . Por exemplo: 1 - Homer Simpson .
+// // Crie uma função que leia todos os dados do arquivo e imprima cada personagem no formato id - Nome . Por exemplo: 1 - Homer Simpson .
 
 function readSimps() {
   return fs.readFile('./simpsons.json', 'utf-8')
@@ -15,22 +15,26 @@ function readSimps() {
     })
 }
 
-readSimps();
+// readSimps();
 // // Crie uma função que receba o id de uma personagem como parâmetro e retorne uma Promise que é resolvida com os dados da personagem que possui o id informado. Caso não haja uma personagem com o id informado, rejeite a Promise com o motivo "id não encontrado".
 
 async function getSimpsons(id) {
   const simpsons = await fs.readFile('./simpsons.json', 'utf-8')
-  .then((fileName) => JSON.parse(fileName));
+  .then((fileName) => JSON.parse(fileName))
+  // .then((simps) => simps.find((char) => char.id === id))
+  // .then((character) => console.log('Id encontrado', character) || character)
 
-  const findId = simpsons.find((simps) => simps.id === id)
+  const findId = simpsons.find((simps) => simps.id == id)
 
-  if (findId) {
-    throw new Error('id nao encontrado')
+  if (!findId) {
+    return new Error('id nao encontrado')
   }
   return findId
 }
 
-getSimpsons(3);
+getSimpsons(15)
+  .then(result => console.log(result))
+  .catch(err => console.log(err.message));
 
 // // Crie uma função que altere o arquivo simpsons.json retirando os personagens com id 10 e 6.
 
@@ -45,7 +49,7 @@ async function removeSimpsons() {
 removeSimpsons();
 
 
-// Crie uma função que leia o arquivo simpsons.json e crie um novo arquivo, chamado simpsonFamily.json , contendo as personagens com id de 1 a 4.
+// // Crie uma função que leia o arquivo simpsons.json e crie um novo arquivo, chamado simpsonFamily.json , contendo as personagens com id de 1 a 4.
 async function createSimpsonsFamily() {
   const simpsons = await fs
     .readFile('./simpsons.json', 'utf-8')
@@ -56,8 +60,9 @@ async function createSimpsonsFamily() {
     await fs.writeFile('./simpsonsFamily.json', JSON.stringify(simpsonsFamily));
 }
 
+createSimpsonsFamily();
 
-// Crie uma função que adicione ao arquivo simpsonFamily.json o personagem Nelson Muntz .
+// // Crie uma função que adicione ao arquivo simpsonFamily.json o personagem Nelson Muntz .
 async function addNelsonToFamily() {
   const simpsonsFamily = await fs
     .readFile('./simpsonsFamily.json', 'utf-8')
@@ -67,8 +72,10 @@ async function addNelsonToFamily() {
     await fs.writeFile('./simpsonsFamily.json', simpsonsFamily);
 }
 
-// Crie uma função que substitua o personagem Nelson Muntz pela personagem Maggie Simpson no arquivo simpsonFamily.json .
-// Importamos o módulo de promises do fs
+addNelsonToFamily();
+
+// // Crie uma função que substitua o personagem Nelson Muntz pela personagem Maggie Simpson no arquivo simpsonFamily.json .
+// // Importamos o módulo de promises do fs
 function replaceNelson () {
   // Realizamos a leitura do arquivo
   return fs.readFile('./simpsonsFamily.json', 'utf-8')
@@ -83,3 +90,5 @@ function replaceNelson () {
     .then((simpsonsWithMaggie) =>
       fs.writeFile('./simpsonsFamily.json', JSON.stringify(simpsonsWithMaggie)));
 }
+
+replaceNelson();
